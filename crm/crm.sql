148eb6eb630eb70a79a8e4f76908d8e3ef012cfe
alyssamarcus <alyssa.marcus@kellogg.northwestern.edu>
2022-04-15 21:00:37 +0000

    school

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,39 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+
+DROP TABLE IF EXISTS salesperson;
+DROP TABLE IF EXISTS company;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS activity;
+
+CREATE TABLE salesperson (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    first_name TEXT,
+    last_name TEXT, 
+    email TEXT
+);
+
+CREATE TABLE company (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+
+CREATE TABLE customers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT, 
+    last_name TEXT, 
+    company_id TEXT,
+    email TEXT, 
+    phone TEXT,
+    title TEXT
+);
+
+CREATE TABLE activity (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT, 
+    created at TEXT, 
+    customer_id INTEGER,
+    salesperson_id INTEGER,
+    notes TEXT
+);
